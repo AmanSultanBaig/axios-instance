dfcb5fdcfc0553d3df4eeff374edee9572242483
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-06-07 13:00:13 +0500

    Update axios-instance.js

--- axios-instance.js
+++ axios-instance.js
@@ -1,14 +1,17 @@
-import axios from 'axios'
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL: `URL`,
-})
+  baseURL: ``,
+});
 
 // Set the AUTH token for any request
 instance.interceptors.request.use(function (config) {
-    const token = localStorage.getItem(AUTH_TOKEN_KEY)
-    config.headers.Authorization = token ? `Bearer ${token}` : "";
-    return config;
+  const token = localStorage.getItem("");
+  config.headers.Authorization = token ? `Bearer ${token}` : "";
+  return config;
 });
 
-export default instance;
+export const create = (url, body) => instance.post(url, body)
+export const update = (url, body) => instance.put(url, body)
+export const remove = (url) => instance.delete(url)
+export const get = (url) => instance.get(url);
